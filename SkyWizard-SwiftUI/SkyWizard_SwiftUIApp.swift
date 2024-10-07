1cd3c5661731172840a6a9d7aefca41723be4442
Hishara Dilshan <hisharadilshan3@gmail.com>
2024-10-07 17:41:53 +0100

    Initial Commit

---
+++ SkyWizard-SwiftUI/SkyWizard_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyWizard_SwiftUIApp.swift
+//  SkyWizard-SwiftUI
+//
+//  Created by Hishara Dilshan on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SkyWizard_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
