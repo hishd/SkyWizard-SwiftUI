7754476f999c8a169fe328550fea7541caa5d80a
Hishara Dilshan <hisharadilshan3@gmail.com>
2024-11-07 20:09:46 +0000

    Added missing keys for decodable model
    Added missing properties for weather response

--- SkyWizard-SwiftUI/Data/Model/WeatherResult.swift
+++ SkyWizard-SwiftUI/Data/Model/WeatherResult.swift
@@ -10,20 +10,33 @@ import Foundation
 struct WeatherResult: Decodable {
     let latitude: Double
     let longitude: Double
+    let current: CurrentWeatherResponse
     let hourly: HourlyWeatherResponse
     let daily: DailyWeatherResponse
-    
+}
+
+extension WeatherResult {
     struct HourlyWeatherResponse: Decodable {
         let time: [String]
         let temperature_2m: [Double]
         let weather_code: [Int]
     }
-    
+}
+
+extension WeatherResult {
     struct DailyWeatherResponse: Decodable {
         let time: [String]
         let temperature_2m_max: [Double]
         let temperature_2m_min: [Double]
         let weather_code: [Int]
+    }
+}
+
+extension WeatherResult {
+    struct CurrentWeatherResponse: Decodable {
+        let temperature_2m: Double
+        let apparent_temperature: Double
+        let weather_code: Int
     }
 }
 
@@ -43,7 +56,13 @@ extension WeatherResult {
             weather_code: (0...5).map { $0 }
         )
         
-        return .init(latitude: 52.52, longitude: 13.41, hourly: hourlySample, daily: dailySample)
+        let currentSample: CurrentWeatherResponse = .init(
+            temperature_2m: 20.0,
+            apparent_temperature: 12.0,
+            weather_code: 3
+        )
+        
+        return .init(latitude: 52.52, longitude: 13.41, current: currentSample, hourly: hourlySample, daily: dailySample)
     }
     #endif
 }
