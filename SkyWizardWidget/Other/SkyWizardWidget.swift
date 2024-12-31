7278bda82256d54f2f3fe48af5790a8572b35b73
Hishara Dilshan <hisharadilshan3@gmail.com>
2024-12-31 16:28:28 +0000

    Extracted views

---
+++ SkyWizardWidget/Other/SkyWizardWidget.swift
@@ -0,0 +1,58 @@
+//
+//  SkyWizardWidget.swift
+//  SkyWizardWidgetExtension
+//
+//  Created by Hishara Dilshan on 31/12/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> WeatherEntry {
+        .sample
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (WeatherEntry) -> ()) {
+        let entry: WeatherEntry = .sample
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [WeatherEntry] = []
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+
+//    func relevances() async -> WidgetRelevances<Void> {
+//        // Generate a list containing the contexts this widget is relevant in.
+//    }
+}
+
+struct SkyWizardWidget: Widget {
+    let kind: String = "SkyWizard Weather"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                SkyWizardWidgetEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                SkyWizardWidgetEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .supportedFamilies([.systemMedium])
+        .configurationDisplayName("Weather Widget")
+        .description("Weather Widget for SkyWizard")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    SkyWizardWidget()
+} timeline: {
+    WeatherEntry.sample
+    WeatherEntry.sample
+}
--- SkyWizardWidget/Presentation/WeatherWidget.swift
+++ SkyWizardWidget/Presentation/WeatherWidget.swift
@@ -5,30 +5,7 @@
 //  Created by Hishara Dilshan on 29/12/2024.
 //
 
-import WidgetKit
 import SwiftUI
-
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> WeatherEntry {
-        .sample
-    }
-
-    func getSnapshot(in context: Context, completion: @escaping (WeatherEntry) -> ()) {
-        let entry: WeatherEntry = .sample
-        completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [WeatherEntry] = []
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
-
-//    func relevances() async -> WidgetRelevances<Void> {
-//        // Generate a list containing the contexts this widget is relevant in.
-//    }
-}
 
 struct SkyWizardWidgetEntryView : View {
     var entry: Provider.Entry
@@ -42,31 +19,4 @@ struct SkyWizardWidgetEntryView : View {
             Text(entry.city)
         }
     }
-}
-
-struct WeatherWidget: Widget {
-    let kind: String = "SkyWizard Weather"
-
-    var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            if #available(iOS 17.0, *) {
-                SkyWizardWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
-            } else {
-                SkyWizardWidgetEntryView(entry: entry)
-                    .padding()
-                    .background()
-            }
-        }
-        .supportedFamilies([.systemMedium])
-        .configurationDisplayName("Weather Widget")
-        .description("Weather Widget for SkyWizard")
-    }
-}
-
-#Preview(as: .systemSmall) {
-    WeatherWidget()
-} timeline: {
-    WeatherEntry.sample
-    WeatherEntry.sample
 }
