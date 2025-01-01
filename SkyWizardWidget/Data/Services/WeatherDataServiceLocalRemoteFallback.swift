0718b9dea3cbe5df315eb24884330e06548806a5
Hishara Dilshan <hisharadilshan3@gmail.com>
2025-01-01 22:18:52 +0000

    Added concrete type to handle fallback

---
+++ SkyWizardWidget/Data/Services/WeatherDataServiceLocalRemoteFallback.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherDataServiceLocalRemoteFallback.swift
+//  SkyWizardWidgetExtension
+//
+//  Created by Hishara Dilshan on 01/01/2025.
+//
+
+import Foundation
+
+struct WeatherDataServiceLocalRemoteFallback: WeatherDataService {
+    
+    let localDataService: WeatherDataService
+    let remoteDataService: WeatherDataService
+    
+    func getWeatherData() throws(WeatherServiceError) -> WeatherEntry {
+        //if connection available -> remote
+        //else -> local
+        //if error -> throw unavailable
+        .sample
+    }
+}
