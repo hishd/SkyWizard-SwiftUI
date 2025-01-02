9d3bf754cf209cd25649e55e49308f5c545b8e5e
Hishara Dilshan <hisharadilshan3@gmail.com>
2025-01-02 10:36:10 +0000

    Added factory to manage dependencies

---
+++ SkyWizardWidget/Application/Dependencies.swift
@@ -0,0 +1,13 @@
+//
+//  Dependencies.swift
+//  SkyWizardWidgetExtension
+//
+//  Created by Hishara Dilshan on 02/01/2025.
+//
+
+import Foundation
+import Factory
+
+extension Container {
+    
+}
